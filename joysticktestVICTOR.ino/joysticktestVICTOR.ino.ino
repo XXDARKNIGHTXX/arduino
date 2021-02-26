f1cfc939a18bd58a685b43868eb9eb81fa5bff51
Victor Rodriguez <78345788+XXDARKNIGHTXX@users.noreply.github.com>
2021-02-26 11:44:51 +0100

    Add files via upload

---
+++ joysticktestVICTOR.ino/joysticktestVICTOR.ino.ino
@@ -0,0 +1,22 @@
+const  int pinBoton = 3 ;
+const  int pinEjeY = A1;
+const  int pinEjeX = A0;
+ 
+ configuración vacía () {
+  pinMode (pinBoton, ENTRADA);
+  digitalWrite (pinBoton, ALTO);
+  De serie. comenzar ( 9600 );
+}
+ 
+ bucle vacío () {
+  De serie. imprimir ( " Boton:   " );
+  De serie. imprimir ( digitalRead (pinBoton));
+  De serie. imprimir ( " \ n " );
+  De serie. imprimir ( " Eje X: " );
+  De serie. imprimir ( analogRead (pinEjeX));
+  De serie. imprimir ( " \ n " ); // esto es un salto de linea
+  De serie. print ( " Eje Y: " );
+  De serie. println ( analogRead (pinEjeY));
+  De serie. imprimir ( " \ n \ n " );
+  retraso ( 150 );
+}
